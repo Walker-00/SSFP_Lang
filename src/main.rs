0f2aa7296cf4376a1ae940e6751811c3020f87f2
Linus Walker <w000alker@gmail.com>
2023-07-05 07:45:49 +0630

    Making Tokens

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io::{stdin, Read};
+
+enum Token {
+    TOKEOF = -1,
+    TOKDEF = -2,
+    TOKEXTERN = -3,
+    TOKIDENTIFIER = -4,
+    TOKNUMBER = -5,
+}
+
+static mut IDENTIFIERSTR: String = String::new();
+static mut NUMVAL: f64 = 0.;
+
+fn gettok() -> i32 {
+    let mut last_char: u8 = b' ';
+    while last_char.is_ascii_whitespace() {
+        let mut input = [0; 1];
+        stdin().read_exact(&mut input).unwrap();
+    }
+    2
+}
+
 fn main() {
     println!("Hello, world!");
 }
